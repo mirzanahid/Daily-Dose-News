df8e1dcc894c2749a3b075cce516e212086ef596
mirzanahid <mirzasaikat3@gmail.com>
2022-09-03 16:24:08 +0600

    desing complete

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// get categories
+
+// const getCategories = async ()=>{
+//     const url = `https://openapi.programming-hero.com/api/news/categories`;
+//     try{
+
+//         const res = await fetch(url);
+//         const data = await res.json();
+//         console.log(data);
+
+
+//     }
+//     catch(error){
+//        console.log(error);
+//     }
+    
+// }
